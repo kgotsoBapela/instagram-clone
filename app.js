1d8101029ab0cea0f6c0f9ac8303cc11a0c06e34
Kgotso Bapela <kgotso.bapela@gmail.com>
2023-08-16 11:17:54 +0200

    auth updated

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ function redirectToAuth(){
     signInOptions: [
       {
         provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
-        signInMethod: firebase.auth.EmailAuthProvider.EMAIL_LINK_SIGN_IN_METHOD
+        requireDisplayName: false
       }
     ],
     // Other config options...
